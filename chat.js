08f263265434ff37bb510413abdfc7e0f55e4def
Soung jin Ryu <rsj11201@gmail.com>
2020-03-06 15:49:37 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,11 +2,11 @@ var chatClient = function chatClient(options){
 	if(!options.password){
 		this.password = "SCHMOOPIIE";//익명의 사용자
 		this.username = "justinfan"+makeRandom(1,65535);//참고 https://inspect.cool/2018/08/31/twitch/
-	  this.port=80;
+		this.port=80;
 	}else{
 		this.username = options.username;
 		this.password = options.password;
-		this.port=443 ;
+	  this.port=443;
 	}
 	console.log(this.username+"으로 접속시도...");
 	this.isMood=false;
@@ -14,31 +14,81 @@ var chatClient = function chatClient(options){
   this.server='irc-ws.chat.twitch.tv';
 }
 chatClient.prototype.open = function open(){
-	this.webSocket = new WebSocket('wss://'+this.server+(this.port==80?'':':'+this.port));
+	var target = 'wss://' + this.server + (this.port==80?'':':'+this.port);
+	if(this.port==80)
+		this.webSocket = new WebSocket(target,'irc');
+	else this.webSocket = new WebSocket(target,);
 	this.webSocket.onmessage = this.onMessage.bind(this);
 	this.webSocket.onerror = this.onError.bind(this);
 	this.webSocket.onclose = this.onClose.bind(this);
 	this.webSocket.onopen = this.onOpen.bind(this);
+	console.log(target);
 };
 chatClient.prototype.onOpen = function onOpen(){
 	var socket = this.webSocket;
 	if (socket !== null && socket.readyState === 1) {
 		console.log('Connecting and authenticating...');
-		socket.send('CAP REQ :twitch.tv/tags twitch.tv/commands');
-		socket.send('PASS ' + this.password);
+		socket.send('CAP REQ :twitch.tv/tags twitch.tv/commands'+(this.port==80?"":" twitch.tv/membership"));
+		socket.send('PASS oauth:' + this.password);
 		socket.send('NICK ' + this.username);
 		socket.send('JOIN #' + this.channel);
+		this.onConsole("Connecting!");
 	}
+};
+chatClient.prototype.getUser = function(user,func){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function(){if(this.readyState==4&&this.status==200)func(this.responseText)};
+	xmlhttp.open("GET","https://api.twitch.tv/kraken/users?login="+user);
+	xmlhttp.setRequestHeader("accept","application/vnd.twitchtv.v5+json");
+	xmlhttp.setRequestHeader('Authorization','OAuth '+this.password);
+	xmlhttp.send();
+};
+chatClient.prototype.parseMessage = function(rawMessage) {
+    var data = rawMessage.split(";");
+    var parsedMessage = {}
+    if (rawMessage[0] == ':'){
+      data = rawMessage.split(" ");
+      parsedMessage["command"] = data[1];
+      if (parsedMessage["command"] == "JOIN")
+        parsedMessage["message"] =data[0].split("!")[0].substring(1);
+			else parsedMessage["message"] = rawMessage
+    }else if (rawMessage.indexOf("PING") != -1){
+      parsedMessage['PING'] = rawMessage.substring(rawMessage.indexOf(":")+1);
+			setTimeout(function(){
+				this.webSocket.send("PING");
+			},60*1000);
+    }else {
+      for (var i = 0; i < data.length; i++){
+        var d = data[i].split("=");
+        parsedMessage[d[0]] = d[1];
+      }
+      if (parsedMessage.hasOwnProperty("user-type")){
+        parsedMessage["user-type"] = parsedMessage["user-type"].split(":");
+        parsedMessage["user-type"].splice(0, 1);
+        parsedMessage["command"] = parsedMessage["user-type"][0].split(" ")[1]
+      }
+      var message = data[data.length-1].split(":");
+      parsedMessage["message"] = message.slice(2).join(":");
+    }
+  return parsedMessage;
+};
+chatClient.prototype.onSend = function(message){
+  var socket = this.webSocket;
+  if (socket !== null && socket.readyState === 1) {
+      console.log('Send : ' + message);
+      socket.send('PRIVMSG #' + this.channel + " :"+ message);
+  }
 };
 chatClient.prototype.onMessage = function onMessage(message){
   if(message !== null){
-		console.log(message.data);
-      var parsed = this.parseMessage(message.data.replace("\n","").replace("\r",""));
+		var messages = message.data.replace("\r","").split("\n");
+		for(var msg of messages){
+      var parsed = this.parseMessage(msg);
       if(parsed !== null){
         switch(parsed.command){
           case "JOIN":
 						this.onJoin(parsed["message"]);
-          case "USERSTATE":
+          case "USERSTATE"://사용자 참여
             break;
           case "PING":
             this.webSocket.send("PONG :"+parsed['PING']);
@@ -61,94 +111,27 @@ chatClient.prototype.onMessage = function onMessage(message){
             if (parsed["msg-id"] == "highlighted-message")
               this.onHighlighted(parsed.message);
             if (parsed.message[0] == "#" && (parsed["badges"].indexOf("broadcaster") != -1 ||
-						 	parsed["user-id"].indexOf("129955642")!=-1)||(this.isMood && parsed["badges"].indexOf("mod") != -1)){
+						 	parsed["user-id"].indexOf("129955642")!=-1)||(this.isMood && parsed["badges"].indexOf("mod") != -1))//"moderator/1"
               this.onCommand(parsed.message.substring(1).split(" "),parsed);
-							return;
-						}
 						this.onChating(parsed);
             break;
           default:
-            if (parsed["PING"]){
-							this.webSocket.send("PONG :"+parsed['PING']);
-							setTimeout(function(){
-							  var socket = this.webSocket;
-							  if (socket!==null&&socket.readyState===1)
-							      socket.send('PING');
-							},60*1000);
-						}
+            if (parsed["PING"])this.webSocket.send("PONG :"+parsed['PING']);
         }
       }
-  }
-};
-chatClient.prototype.parseMessage = function(rawMessage) {
-    var data = rawMessage.split(";");
-    var parsedMessage = {}
-    if (rawMessage[0] == ':'){
-      data = rawMessage.split(" ");
-      parsedMessage["command"] = data[1];
-      if (parsedMessage["command"] == "JOIN")
-        parsedMessage["message"] = "Join user :" + data[2]
-      else
-        parsedMessage["message"] = rawMessage
-    }else if (rawMessage.indexOf("PING") != -1){
-      parsedMessage['PING'] = rawMessage.substring(rawMessage.indexOf(":")+1);
-      console.log(parsedMessage['PING'])
-    }else {
-      for (var i = 0; i < data.length; i++){
-        var d = data[i].split("=");
-        parsedMessage[d[0]] = d[1];
-      }
-      if (parsedMessage.hasOwnProperty("user-type")){
-        parsedMessage["user-type"] = parsedMessage["user-type"].split(":");
-        parsedMessage["user-type"].splice(0, 1);
-        parsedMessage["command"] = parsedMessage["user-type"][0].split(" ")[1]
-      }
-      var message = data[data.length-1].split(":");
-      parsedMessage["message"] = message.slice(2).join(":");
-    }
-  return parsedMessage;
-};
-chatClient.prototype.parseTwitchEmoticon = function(message, emotes){
-
-}
-chatClient.prototype.parseTwitchEmoticon = function(message, emotes){
-	let ranges, id, emote_id, regExp;
-	const replace_list = {};
-
-	if (typeof emotes != 'undefined') {
-		const emote_list = emotes.split("/");
-		emote_list.forEach(function (emote_replace) {
-			ranges = emote_replace.split(":");
-			id = ranges[0];
-			if (typeof ranges[1] == 'undefined') return;
-			ranges = ranges[1].split(",");
-			if (typeof ranges[0] != 'undefined') {
-				ranges = ranges[0].split("-");
-				emote_id = message.substring(parseInt(ranges[0]), parseInt(ranges[1]) + 1);
-				replace_list[emote_id] = id;
-			}
-		});
-		for (const replace_id in replace_list) {
-			regExp = new RegExp(escapeRegExp(replace_id), "g");
-			message = message.replace(regExp, "");
 		}
-	}
-	return message;
-}
-chatClient.prototype.onSend = function(message){
-  var socket = this.webSocket;
-  if (socket !== null && socket.readyState === 1)
-      socket.send('PRIVMSG ' + this.channel + " :"+ message);
+  }
 };
 chatClient.prototype.onError = function onError(message){console.log('Error: '+message)};
 chatClient.prototype.onChating = function(parsed){console.log(parsed)};
 chatClient.prototype.onEmotes = function(parsed,count){console.log(parsed)};
 chatClient.prototype.onHighlighted = function(message){console.log(message)};
 chatClient.prototype.onBits = function(bit,name,message){console.log(message)};
+chatClient.prototype.onJoin = function(message){console.log(message)};
+chatClient.prototype.onConsole = function(message){console.log(message)};
 chatClient.prototype.onCommand = function(message,parsed){console.log(message)};
 chatClient.prototype.onClose = function(){console.log('Disconnected from the chat server.');};
 chatClient.prototype.close = function(){if(this.webSocket)this.webSocket.close()};
-
 function permiss(){//https://lastorder.xyz/chatreader-kor/speech.html 참고
 	if (document.location.hash!==""&&document.location.hash.indexOf("access_token")!==-1){
 		const rawauth = document.location.href.replace("#", "?");
