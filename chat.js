f804aa1dc0062a5e9534db5611b626c784f606aa
Soung jin Ryu <rsj11201@gmail.com>
2020-03-06 13:56:44 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -32,6 +32,7 @@ chatClient.prototype.onOpen = function onOpen(){
 };
 chatClient.prototype.onMessage = function onMessage(message){
   if(message !== null){
+		console.log(message);
       var parsed = this.parseMessage(message.data.replace("\n","").replace("\r",""));
       if(parsed !== null){
         switch(parsed.command){
